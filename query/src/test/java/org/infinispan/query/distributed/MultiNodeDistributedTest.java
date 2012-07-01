daf8ee69bac635e02fc7aafa27aaafa9e82a0bee
Sanne Grinovero <sanne@hibernate.org>
2012-07-01 19:49:21 +0100

    Initialize all caches and wait for state transfers

--- query/src/test/java/org/infinispan/query/distributed/MultiNodeDistributedTest.java
+++ query/src/test/java/org/infinispan/query/distributed/MultiNodeDistributedTest.java
@@ -18,6 +18,7 @@
  */
 package org.infinispan.query.distributed;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.TransactionManager;
@@ -25,14 +26,17 @@ import javax.transaction.TransactionManager;
 import junit.framework.Assert;
 
 import org.apache.lucene.search.MatchAllDocsQuery;
+import org.hibernate.search.infinispan.impl.InfinispanDirectoryProvider;
 import org.infinispan.Cache;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.query.CacheQuery;
 import org.infinispan.query.Search;
 import org.infinispan.query.SearchManager;
 import org.infinispan.query.test.Person;
 import org.infinispan.test.MultipleCacheManagersTest;
+import org.infinispan.test.TestingUtil;
 import org.testng.annotations.Test;
 
 /**
@@ -47,24 +51,41 @@ public class MultiNodeDistributedTest extends MultipleCacheManagersTest {
    @Override
    protected void createCacheManagers() throws Throwable {
       ConfigurationBuilder cacheCfg = getDefaultClusteredCacheConfig(CacheMode.DIST_SYNC, transactionsEnabled());
-      cacheCfg.indexing()
-         .enable()
-         //only originator of each write takes care for index write
-         .indexLocalOnly(true)
-         //use our custom IndexManager to wire up the remoting delegation via custom commands
-         .addProperty("hibernate.search.default.indexmanager", org.infinispan.query.indexmanager.InfinispanIndexManager.class.getName())
-         //specify the managed index is to be shared across the nodes
-         .addProperty("hibernate.search.default.directory_provider", "infinispan")
-         .addProperty("hibernate.search.​default.​exclusive_index_use", "false")
-         .addProperty("hibernate.search.default.reader.strategy", "not-shared")
-         .addProperty("hibernate.search.lucene_version", "LUCENE_CURRENT");
+      cacheCfg
+         .clustering()
+            .stateTransfer()
+               .fetchInMemoryState(true)
+         .indexing()
+            .enable()
+            //only originator of each write takes care for index write
+            .indexLocalOnly(true)
+            //use our custom IndexManager to wire up the remoting delegation via custom commands
+            .addProperty("hibernate.search.default.indexmanager", org.infinispan.query.indexmanager.InfinispanIndexManager.class.getName())
+            //specify the managed index is to be shared across the nodes
+            .addProperty("hibernate.search.default.directory_provider", "infinispan")
+            .addProperty("hibernate.search.​default.​exclusive_index_use", "false")
+            .addProperty("hibernate.search.default.reader.strategy", "not-shared")
+            .addProperty("hibernate.search.lucene_version", "LUCENE_CURRENT");
       List<Cache<String, Person>> caches = createClusteredCaches(2, cacheCfg);
       cache1 = caches.get(0);
       cache2 = caches.get(1);
+      completeAllCaches(InfinispanDirectoryProvider.DEFAULT_INDEXESDATA_CACHENAME);
+      completeAllCaches(InfinispanDirectoryProvider.DEFAULT_INDEXESMETADATA_CACHENAME);
+      completeAllCaches(InfinispanDirectoryProvider.DEFAULT_LOCKING_CACHENAME);
+   }
+
+   private void completeAllCaches(String cacheName) {
+      EmbeddedCacheManager cm1 = cacheManagers.get(0);
+      EmbeddedCacheManager cm2 = cacheManagers.get(0);
+      List cacheList = new ArrayList(2);
+      cacheList.add(cm1.getCache(cacheName));
+      cacheList.add(cm2.getCache(cacheName));
+      TestingUtil.blockUntilViewsReceived(10000, cacheList);
+      TestingUtil.waitForRehashToComplete(cacheList);
    }
 
    protected boolean transactionsEnabled() {
-      return false;
+      return true;
    }
 
    private void storeOn(Cache<String, Person> cache, String key, Person person) throws Exception {
@@ -78,11 +99,11 @@ public class MultiNodeDistributedTest extends MultipleCacheManagersTest {
    public void testSimple() throws Exception {
       assertIndexSize(0);
       //depending on test run, the index master selection might pick either cache
-      storeOn(cache1, "k1", new Person("K. Firt", "Is not a characted from the matrix", 1));
+      storeOn(cache1, "k1", new Person("K. Firt", "Is not a character from the matrix", 1));
       assertIndexSize(1);
-      storeOn(cache1, "k2", new Person("K. Seycond", "Is a pilot", 1));
+      storeOn(cache2, "k2", new Person("K. Seycond", "Is a pilot", 1));
       assertIndexSize(2);
-      storeOn(cache2, "k3", new Person("K. Theerd", "Forgot the fundamental laws", 1));
+      storeOn(cache1, "k3", new Person("K. Theerd", "Forgot the fundamental laws", 1));
       assertIndexSize(3);
    }
 
